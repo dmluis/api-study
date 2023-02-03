3cd931d75e9819e6119dc357efbd39acf082a26d
LuisD <lamdomiciano@gmail.com>
2023-02-03 18:15:06 -0300

    feat: user model

---
+++ src/main/java/com/luisd/apistudy/models/UserModel.java
@@ -0,0 +1,84 @@
+package com.luisd.apistudy.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class UserModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty(message = "Field name is required")
+    @Length(min = 3, max = 180, message = "Name should between 3 and 180 characters")
+    private String name;
+    @NotEmpty(message = "Field login is required")
+    @Length(min = 3, max = 100, message = "Login should between 3 and 100 characters")
+    private String login;
+    @NotEmpty(message = "Field password is required")
+    @Length(min = 6, max = 16, message = "Password should between 6 and 16 characters")
+    private String password;
+
+    public UserModel() {
+    }
+
+    public UserModel(Integer id, String name, String login, String password) {
+        this.id = id;
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserModel userModel = (UserModel) o;
+        return Objects.equals(id, userModel.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
