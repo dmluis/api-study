ac36259ca17e3e4cbb304c2748578df0d3f17a39
LuisD <lamdomiciano@gmail.com>
2023-02-03 21:14:45 -0300

    feat: implement CommandLineRun

--- src/main/java/com/luisd/apistudy/ApiStudyApplication.java
+++ src/main/java/com/luisd/apistudy/ApiStudyApplication.java
@@ -1,13 +1,20 @@
 package com.luisd.apistudy;
 
+import com.luisd.apistudy.models.UserModel;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiStudyApplication {
+public class ApiStudyApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiStudyApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		UserModel user = new UserModel(null, "José da Silva", "joseS", "abc123");
+		UserModel user2 = new UserModel(null, "Robert Renam", "rrsscp", "roberHeartz");
+	}
 }
